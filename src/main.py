cf948046dbff16006eb24b8ebad143683037dbbe
eliaskone <56181285+eliaskone@users.noreply.github.com>
2021-05-20 14:53:10 +0200

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+import Controller
+import Model
+import View
+
+if __name__ == '__main__':
+    root = Tk()
+    view = View.View(root)
+    model = Model.Model()
+    controller = Controller.Controller(model, view)
+    controller.loadDataToLBX()
+    root.mainloop()
